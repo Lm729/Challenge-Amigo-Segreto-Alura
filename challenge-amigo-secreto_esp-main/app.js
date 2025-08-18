537fe9b0916c7b8b96d662ace626bb2288db53a6
Leandro Moreno <morenoleandro182@gmail.com>
2025-08-18 14:39:27 -0300

    Modificación de archivos js y html

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+
+function agregarAmigo () {
+    //Accede al ID, valida, quita espacios y convierte a minúscula.
+    let lista = document.getElementById ('amigo').value.trim ().toLowerCase ();
+
+    //Valida si el campo no esta vacio, no sean números, no tengan números y no tenga signos.
+     if (lista === "" || /\d/.test(lista) || /[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/.test(lista)) {
+        alert("Por favor, inserte un nombre.");
+        document.getElementById ('amigo').value = "";
+        return;
+    }
+
+    if (amigos.includes (lista)) {
+        alert ("Ya existe en la lista, agrega otro nombre.");
+        document.getElementById ('amigo').value = "";
+        return;
+    }
+    amigos.push (lista);
+    document.getElementById ('amigo').value = "";
+    console.log (amigos);
+    actualizaLista ();
+}
+
+function actualizaLista () {
+    lista = document.getElementById ('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i ++) {
+        lista.innerHTML += `<li>${amigos [i]}</li>`;
+    }
+}
+
+function sortearAmigo () {
+    let lista = document.getElementById ('resultado');
+    let nombres = document.getElementById ('listaAmigos');
+    let aleatorio = Math.floor (Math.random () * amigos.length);
+    
+    if (!amigos.length) {
+        alert ("No puedes Sortear sin tener nombres.");
+        return;
+    }
+
+    lista.innerHTML = `<li>El amigo secreto es: ${amigos [aleatorio]}</li>`;
+    nombres.innerHTML = "";
+    return;
+}
